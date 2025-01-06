64e6c14f1af91dbd26f685c61e63f98b017ab651
osmanozysr <123673331+OsmanOzyasar@users.noreply.github.com>
2025-01-06 15:23:17 +0300

    Add project files.

---
+++ Bebek.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorPratik
+{
+    public class Bebek
+    {
+        public Bebek() { }
+
+        private string _name;
+        private string _surname;
+        private DateTime _birthDay;
+
+        public Bebek(string name, string surname)
+        {
+            _name = name;
+            _surname = surname;
+
+            _birthDay = DateTime.Now;
+
+            Console.WriteLine($"Ingaaa isim: {name} soyisim: {surname} doğum tarihi: {_birthDay}");
+        }
+
+        public void WriteName(string name, string surname)
+        {
+            _birthDay = DateTime.Now;
+            Console.WriteLine($"Ingaaa isim: {name} soyisim: {surname} doğum tarihi: {_birthDay}");
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Surname
+        {
+            get { return _surname; }
+            set { _surname = value; }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using ConstructorPratik;
+using System;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bebek bebek = new Bebek();
+
+            string name = bebek.Name = "Hüseyin";
+            string surname = bebek.Surname = "Şibidik";
+            bebek.WriteName(name, surname);
+
+            Bebek bebek1 = new Bebek("Mustafa", "Şibidik");
+        }
+    }
+}
